68a51729224d9ce8471b176d22d461d5d945bb8d
nktrnv <nktrnv@gmail.com>
2023-08-20 20:46:17 +0300

    Добавить константы для YDB в конфигурационный файл

--- src/config.py
+++ src/config.py
@@ -1,1 +1,4 @@
 ADMIN_PASSWORD = "secrete"
+YDB_ENDPOINT = "grpcs://ydb.serverless.yandexcloud.net:2135"
+YDB_DATABASE = "/ru-central1/b1gco1s4ir1fq6mdcrf1/etnedgj1te4iojp9gbtt"
+YDB_SERVICE_ACCOUNT_KEY = "key-ydb-sa.json"
